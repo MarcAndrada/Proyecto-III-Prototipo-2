782508eec35a21c484cfc7c6593c22b1e0edab00
Crami Enti <marc.andrada@enti.cat>
2024-11-19 12:19:52 +0100

    Lever Animation

--- Assets/Scripts/InteractableObjects/Objects/LeverObject.cs
+++ Assets/Scripts/InteractableObjects/Objects/LeverObject.cs
@@ -5,12 +5,19 @@ public class LeverObject : InteractableObject
     [Space, Header("Lever"), SerializeField]
     private SlotMachineController controller;
 
+    private Animator animator;
+    protected override void Awake()
+    {
+        base.Awake();
+        animator = GetComponent<Animator>();
+    }
     public override void ActivateObject()
     {
         if (GameManager.Instance.state == GameManager.GameState.PLAYER_TURN 
             && CanInteract())
         {
             controller.SpinWheel();
+            animator.SetTrigger("Move");
             StopHovering();
         }
     }
