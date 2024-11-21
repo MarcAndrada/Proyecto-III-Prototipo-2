8fce74f03b3b5f640465e8e214a0c86a45d0fbc1
Daniel Martinez <daniel.martinez.liu@enti.cat>
2024-11-21 21:13:08 +0100

    Store Done

--- Assets/Scripts/Store/VirtualScreen.cs
+++ Assets/Scripts/Store/VirtualScreen.cs
@@ -10,22 +10,16 @@ public class VirtualScreen : GraphicRaycaster
 
     public override void Raycast(PointerEventData eventData, List<RaycastResult> resultList)
     {        
-        if (screenCamera == null || screenCamera.targetTexture == null)
-        {
-            Debug.LogWarning("Screen Camera or Render Texture not set.");
-            return;
-        }
-        
         Ray ray = eventCamera.ScreenPointToRay(eventData.position);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
             Renderer renderer = hit.collider.GetComponent<Renderer>();
             if (renderer && renderer?.material.mainTexture == screenCamera.targetTexture)
             {
-                Vector3 virtualPos = new Vector3(hit.textureCoord.x, hit.textureCoord.y);
-                virtualPos.x *= screenCamera.targetTexture.width;
-                virtualPos.y *= screenCamera.targetTexture.height;
+                Vector3 virtualPos = new Vector3(
+                    hit.textureCoord.x * screenCamera.targetTexture.width,
+                    hit.textureCoord.y * screenCamera.targetTexture.height
+                );
                 
                 eventData.position = virtualPos;
 
